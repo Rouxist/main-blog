5332336db41ee10248098cbefd433929352bcb00
Rouxist <name.of.squaner@gmail.com>
2025-06-12 17:19:38 +0900

    style: modified hover effect of photo tab elements

--- src/app/_components_gallery/gallery-element.tsx
+++ src/app/_components_gallery/gallery-element.tsx
@@ -26,21 +26,23 @@ export function GalleryElement({
   thumbnail,
 }: Props) {
   return (
-    <div className="md:w-[300px] w-[40vw] md:h-[360px] md:hover:scale-105 md:transition-all md:duration-250">
-      <div className="relative md:h-[400px] flex items-center">
-        <Image
-          src={thumbnail}
-          width={200}
-          height={200}
-          className="w-[40vw] h-[50vw] md:w-[300px] md:h-[360px]"
-          alt={'thumbnail_' + slug}
-        />
-        <div className="absolute md:bottom-4 bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-black to-transparent pointer-events-none" />
-        <div className="absolute md:bottom-8 bottom-4 left-4 text-white md:text-lg text-xs z-10 md:h-[3.5em] truncate max-w-[80%] overflow-hidden whitespace-nowrap">
-          {title}
-        </div>
-        <div className="absolute md:bottom-0 bottom-0 left-4 text-white md:text-base md:block hidden z-10 h-[3.5em]">
-          <DateFormatter dateString={date}></DateFormatter>
+    <div className="md:w-[300px] md:h-[360px] w-[40vw] overflow-hidden">
+      <div className="w-full h-full md:transition-all md:duration-250 md:hover:scale-105">
+        <div className="relative md:h-[360px] flex items-center">
+          <Image
+            src={thumbnail}
+            width={200}
+            height={200}
+            className="md:w-[300px] md:h-[360px] w-[40vw] h-[50vw] "
+            alt={'thumbnail_' + slug}
+          />
+          <div className="absolute md:bottom-0 bottom-0 left-0 right-0 h-2/5 bg-gradient-to-t from-black to-transparent pointer-events-none" />
+          <div className="absolute md:bottom-6 bottom-4 left-4 text-white md:text-xl text-sm z-10 md:h-[2.5em] truncate max-w-[80%] overflow-hidden whitespace-nowrap">
+            {title}
+          </div>
+          <div className="absolute md:bottom-0 bottom-0 left-4 text-white md:text-base md:block hidden z-10 md:h-[2.5em]">
+            <DateFormatter dateString={date}></DateFormatter>
+          </div>
         </div>
       </div>
     </div>
