4bd19a073ddca1cc72c0acabced51fd3ca957c6e
Rouxist <name.of.squaner@gmail.com>
2026-09-06 23:18:07 +0900

    fix(ui): widen blockquote in mobile screen size

--- src/app/_components_post/post-body.tsx
+++ src/app/_components_post/post-body.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-5xl px-8 mx-auto">
+    <div className="max-w-5xl px-4 sm:px-8 mx-auto">
       <div
         id="content-container"
         className={markdownStyles['markdown']}
