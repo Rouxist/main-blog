6d0c01babec68e50b405904ff9dc36114d0ae105
Rouxist <name.of.squaner@gmail.com>
2026-09-16 18:13:53 +0900

    feat(ui): decrease width of portrait image

--- src/app/_components_gallery/travel-image.tsx
+++ src/app/_components_gallery/travel-image.tsx
@@ -38,7 +38,7 @@ export async function TravelImage({
       className={
         thumbnail
           ? 'h-full w-full object-cover transition-transform duration-300 group-hover:scale-105 motion-reduce:transition-none'
-          : 'h-auto w-full'
+          : 'mx-auto h-auto max-h-[80svh] w-auto max-w-full object-contain'
       }
     />
   )
